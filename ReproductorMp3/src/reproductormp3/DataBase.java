00dd935206eed6fbb8acf26509c939a0c890711a
Christian del Angel <christian.lopez190900@gmail.com>
2022-06-17 17:50:55 -0500

    Se agrego la BD y el JDBC, se establece la 1ra conexion

--- ReproductorMp3/src/reproductormp3/DataBase.java
+++ ReproductorMp3/src/reproductormp3/DataBase.java
@@ -1,9 +1,42 @@
 package reproductormp3;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DataBase {
 
-	public DataBase() {
+	
+	public void getQueueData(){
 		
+		/*
+		 *
+		 *
+		 *
+		 *Pendiente como poner la ruta de la base de datos que se encuentra en al proyecto
+		 *
+		 * 
+		 * 
+		*/
+		String url="jdbc:sqlite:dataBase\\reproductor.db";
+		try {
+			Connection connection = DriverManager.getConnection(url);
+			String sql="SELECT * FROM Queue";
+			
+			Statement statement = connection.createStatement();
+			statement.execute(sql);
+			ResultSet result= statement.executeQuery(sql);
+			while(result.next()){
+				String name= result.getString("Nombre");
+				System.out.println("result: "+name);
+			}
+		}catch(SQLException e){
+			System.out.println("Error connection to SQLite database");
+			e.printStackTrace();
+		}
 	}
+
 
 }
