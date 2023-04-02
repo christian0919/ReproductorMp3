2dac759b0194880ebbe86c2a6b02f3b5d53e3b21
Christian del Angel <christian.lopez190900@gmail.com>
2023-04-02 16:24:43 -0600

    mains classes created

---
+++ ManagingBitmapMemory/src/bitmapmemorymanagement/Main.java
@@ -0,0 +1,19 @@
+
+package bitmapmemorymanagement;
+
+import gui.MainView;
+
+/**
+ * @author christian0919
+ */
+public class Main {
+
+	
+	public static void main(String[] args) {
+		
+		//Launch view
+		MainView frame = new MainView();
+		frame.setVisible(true);
+	}
+
+}
